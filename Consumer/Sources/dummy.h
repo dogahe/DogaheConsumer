778bb9c94f5656a8aaefa406832324dfe98d98b1
Behzad Dogahe <dogahe@google.com>
2023-10-13 13:52:50 -0400

    update dummy.h

--- Consumer/Sources/dummy.h
+++ Consumer/Sources/dummy.h
@@ -15,4 +15,4 @@
  */
 
 // Swift Package Manager requires that every target has a header file.
-#pragma mark To the linter: this is an Objective-C header file.
+// #pragma mark To the linter: this is an Objective-C header file.
